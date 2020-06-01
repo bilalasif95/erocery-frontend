27632f5ce4755e97730edc9acb26b7e352a33bf4
Bilal Asif <bilal@rnssol.com>
2020-06-01 15:42:43 +0500

    redirection to top issue in checkout page resolved

--- src/history.ts
+++ src/history.ts
@@ -3,10 +3,20 @@ import { createBrowserHistory } from "history";
 const history = createBrowserHistory();
 history.listen((_location, action) => {
   if (["PUSH"].includes(action)) {
-    window.scroll({
-      behavior: "smooth",
-      top: 0,
-    });
+    if(_location.pathname === "/checkout/shipping-options/"
+    || _location.pathname === "/checkout/billing-address/"
+    || _location.pathname === "/checkout/payment/"
+    ){
+      window.scroll({
+        behavior: "smooth",
+      });
+    }
+    else{
+      window.scroll({
+        behavior: "smooth",
+        top: 0,
+      });
+    }
   }
 });
 
