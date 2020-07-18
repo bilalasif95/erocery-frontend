2fbfcfab2e04c6940d3fa15a58bdf16bc9e91287
aniqmir <aniqmir@gmail.com>
2020-07-18 17:17:34 +0500

    erocery product variant issue resolved

--- src/@next/components/molecules/OrderTabel/OrderTabel.tsx
+++ src/@next/components/molecules/OrderTabel/OrderTabel.tsx
@@ -11,7 +11,6 @@ import * as S from "./styles";
 import { IProps } from "./types";
 
 export const OrderTabel: React.FC<IProps> = ({ orders, history }: IProps) => {
-
   return (
     <S.Wrapper>
       <table>
@@ -27,38 +26,51 @@ export const OrderTabel: React.FC<IProps> = ({ orders, history }: IProps) => {
         <tbody>
           {orders &&
             orders.map(order => {
-              const apiDate = order.node.created.slice(0, 10)
+              const apiDate = order.node.created.slice(0, 10);
               const date = new Date(apiDate);
               return (
-                <tr data-testid="order__row"
-                key={order.node.number}
-                onClick={evt => {
-                  evt.stopPropagation();
-                  history.push(`/order-history/${order.node.token}`);
-                }}>
+                <tr
+                  data-testid="order__row"
+                  key={order.node.number}
+                  onClick={evt => {
+                    evt.stopPropagation();
+                    order.node.lines[0].variant
+                      ? history.push(`/order-history/${order.node.token}`)
+                      : console.log("err");
+                  }}
+                >
                   <td>{order.node.number}</td>
-                  <td> {order.node.lines
-                    .slice(0, 5)
-                    .map((product: any) => (
+                  <td>
+                    {" "}
+                    {order.node.lines.slice(0, 5).map((product: any) => (
                       <span
                         className="proImg"
-                        key={product.variant.product.id}
+                        key={product.variant && product.variant.product.id}
                         onClick={evt => {
                           evt.stopPropagation();
-                          history.push(
-                            generateProductUrl(
-                              product.variant.product.id,
-                              product.variant.product.name
-                            )
-                          );
+                          product.variant
+                            ? history.push(
+                                generateProductUrl(
+                                  product.variant && product.variant.product.id,
+                                  product.variant &&
+                                    product.variant.product.name
+                                )
+                              )
+                            : console.log("err");
                         }}
                       >
                         <Thumbnail source={product} />
                       </span>
-                    ))}</td>
-                  <td> {`${date.getMonth() +
-                    1}/${date.getDate()}/${date.getFullYear()}`}</td>
-                  <td><TaxedMoney taxedMoney={order.node.total} /></td>
+                    ))}
+                  </td>
+                  <td>
+                    {" "}
+                    {`${date.getMonth() +
+                      1}/${date.getDate()}/${date.getFullYear()}`}
+                  </td>
+                  <td>
+                    <TaxedMoney taxedMoney={order.node.total} />
+                  </td>
                   <td>{order.node.statusDisplay}</td>
                 </tr>
               );
