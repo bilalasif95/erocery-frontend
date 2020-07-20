73eab801e947f0fec7c688bb2ed1b0650a936db0
Bilal Asif <bilal@rnssol.com>
2020-07-20 17:56:38 +0500

    dynamic discount percentage added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import { RouteComponentProps } from "react-router";
 import ReactSVG from "react-svg";
 
 import { Footer, MainMenu, MetaConsumer, OverlayManager } from "../components";
-import { isPath } from "../core/utils";
+import { isPath,maybe } from "../core/utils";
 import { orderConfirmationUrl, Routes } from "./routes";
 
 // import MessengerCustomerChat from "react-messenger-customer-chat";
@@ -13,6 +13,8 @@ import { orderConfirmationUrl, Routes } from "./routes";
 import messengerImage from "../images/facebook-messenger-icon.svg";
 import closeImg from "../images/x.svg";
 // import { appId,pageId } from "../config/index";
+
+import { TypedFeaturedProductsQuery } from "../components/ProductsFeatured/queries";
 
 const App: React.FC<RouteComponentProps> = ({
   history: {
@@ -36,36 +38,52 @@ const App: React.FC<RouteComponentProps> = ({
       <Routes />
       {/* {window.innerWidth < 768 ?  */}
       <a target="_blank" rel="noopener noreferrer" className="messenger" href="https://www.m.me/109977713975774?source=customer_chat_plugin"><ReactSVG path={messengerImage} /></a>
-      {window.innerWidth > 768 && offerBox && 
-          <div className="offer">
-            <div className="close">
-              <div className="inner">
-                <div className="dottedBorder">
-                  <p>Limited Time Offer:</p>
-                  <p className="offOnStock">2% Off</p>
-                  <p>on Entire Stock.</p>
+      <TypedFeaturedProductsQuery displayError={false}>
+        {({ data }) => {
+          const products = maybe(
+            () => data.shop.homepageCollection.products.edges,
+            []
+          );
+        if (products.length) {
+          return (
+            <>
+              {window.innerWidth > 768 && offerBox && products[1].node.pricing && products[1].node.pricing.onSale &&
+                <div className="offer">
+                  <div className="close">
+                    <div className="inner">
+                      <div className="dottedBorder">
+                        <p>Limited Time Offer:</p>
+                        <p className="offOnStock">{(100-((products[1].node.pricing.priceRange.start.gross.amount/products[1].node.pricing.priceRangeUndiscounted.start.gross.amount)*100)).toFixed(0)}% Off</p>
+                        <p>on Entire Stock.</p>
+                      </div>
+                    </div>
+                    <ReactSVG className="closeSVG" onClick={()=> setOfferBox(false)} path={closeImg} />
+                  </div>
                 </div>
-              </div>
-              <ReactSVG className="closeSVG" onClick={()=> setOfferBox(false)} path={closeImg} />
-            </div>
-          </div>
-      }
-      {window.innerWidth < 768 && offerMobileBox &&
-        <div className="mobileOffer">
-          <div className="content">
-            <div className="close">
-              <div className="inner">
-                <div className="dottedBorder">
-                  <p>Limited Time Offer:</p>
-                  <p className="offOnStock">2% Off</p>
-                  <p>on Entire Stock.</p>
+              }
+              {window.innerWidth < 768 && offerMobileBox && products[1].node.pricing && products[1].node.pricing.onSale &&
+                <div className="mobileOffer">
+                  <div className="content">
+                    <div className="close">
+                      <div className="inner">
+                        <div className="dottedBorder">
+                          <p>Limited Time Offer:</p>
+                          <p className="offOnStock">{(100-((products[1].node.pricing.priceRange.start.gross.amount/products[1].node.pricing.priceRangeUndiscounted.start.gross.amount)*100)).toFixed(0)}% Off</p>
+                          <p>on Entire Stock.</p>
+                        </div>
+                      </div>
+                      <ReactSVG className="closeSVG" onClick={()=> setMobileOfferBox(false)} path={closeImg} />
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <ReactSVG className="closeSVG" onClick={()=> setMobileOfferBox(false)} path={closeImg} />
-            </div>
-          </div>
-        </div>
-      }
+              }
+            </>
+            );
+          } else {
+            return null;
+          }
+        }}
+      </TypedFeaturedProductsQuery>
       {/* : <MessengerCustomerChat pageId={pageId} appId={appId} /> } */}
       {!orderConfirmationPage && <Footer />}
       <OverlayManager />
