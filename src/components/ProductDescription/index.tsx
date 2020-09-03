1f96eee205d5f6fb986099c0d1861453d83ead5e
Bilal Asif <bilal@rnssol.com>
2020-09-03 12:44:17 +0500

    http links changed to https

--- src/components/ProductDescription/index.tsx
+++ src/components/ProductDescription/index.tsx
@@ -213,7 +213,7 @@ class ProductDescription extends React.Component<
           <span className="selectedate">{value}</span>
         )}
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           width="20"
           height="20"
           viewBox="0 0 24 24"
