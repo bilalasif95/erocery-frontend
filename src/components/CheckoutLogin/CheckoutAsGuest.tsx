99fb2fa53b2636c452360d67c4fdf0a57a470745
Bilal Asif <bilal@rnssol.com>
2020-12-14 11:44:26 +0500

    added recaptcha

--- src/components/CheckoutLogin/CheckoutAsGuest.tsx
+++ src/components/CheckoutLogin/CheckoutAsGuest.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import ReCAPTCHA from "react-google-recaptcha";
 import { Link } from "react-router-dom";
 import { Button } from "..";
 // import { OverlayTheme, OverlayType } from "..";
@@ -8,18 +9,41 @@ import { OverlayContextInterface } from "../Overlay";
 const CheckoutAsGuest: React.FC<{
   overlay: OverlayContextInterface;
   checkoutUrl: string;
-}> = ({ overlay, checkoutUrl }) => (
-  <div className="checkout-login__guest">
-    <h3 className="checkout__header">Continue as a guest</h3>
-    <p>
-      If you don’t wish to register an account, don’t worry. You can checkout as
-      a guest. We care about you just as much as any registered user.
+}> = ({ overlay, checkoutUrl }) => {
+  const [value, setValue] = React.useState("")
+  const onCaptchaHandler = (value) => {
+    setValue(value)
+  };
+  return (
+    <div className="checkout-login__guest">
+      <h3 className="checkout__header">Continue as a guest</h3>
+      <p>
+        If you don’t wish to register an account, don’t worry. You can checkout as
+        a guest. We care about you just as much as any registered user.
     </p>
-    <Link to={checkoutUrl}>
-      <Button>Continue as a guest</Button>
-    </Link>
+      <div className="recaptcha">
+        <ReCAPTCHA
+          style={{
+            transform: "scale(0.77)",
+            transformOrigin: "0 0",
+          }}
+          className="g-recaptcha"
+          data-theme="light"
+          // sitekey="6LdbyQUaAAAAAAt-FT1vUlibUgfFtqQlEDGSKyf6"
+          sitekey="6LdUyQUaAAAAAC34aQaS-umjwaVjMbJai2d0XyX5"
+          onChange={onCaptchaHandler}
+          height="100px"
+          width="100%"
+        />
+      </div>
+      {value ?
+        <Link to={checkoutUrl}>
+          <Button>Continue as a guest</Button>
+        </Link>
+        : <Button disabled>Continue as a guest</Button>}
 
-    {/* <p>
+
+      {/* <p>
       You can also{" "}
       <span
         className="u-link"
@@ -28,7 +52,8 @@ const CheckoutAsGuest: React.FC<{
         create an account
       </span>
     </p> */}
-  </div>
-);
+    </div>
+  )
+};
 
 export default CheckoutAsGuest;
